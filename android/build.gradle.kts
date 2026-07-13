d53b4a1244d464502bd8b58d8a34485b6f3e5dc2
Fazar Rizwanul Ikhlas <fazarrizwanul@gmail.com>
2026-07-14 02:15:16 +0700

    fix: perbaikan tipe data targetSdkVersion pada skrip gradle root

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,7 +10,7 @@ allprojects {
     }
 }
 
-
+// Konfigurasi direktori output build Flutter
 val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get().asFile
 rootProject.layout.buildDirectory.set(newBuildDir)
 
@@ -18,13 +18,14 @@ subprojects {
     project.layout.buildDirectory.set(newBuildDir.resolve(project.name))
 }
 
-
+// PERBAIKAN FINAL: Menggunakan pemanggilan fungsi targetSdkVersion(35) 
+// tanpa operator '=' agar lolos dari validasi ApiVersion di server GitHub
 subprojects {
     afterEvaluate {
         val androidExtension = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
         if (androidExtension != null) {
             androidExtension.compileSdkVersion(35)
-            androidExtension.defaultConfig.targetSdkVersion = 35
+            androidExtension.defaultConfig.targetSdkVersion(35)
         }
     }
 }
